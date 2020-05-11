7d235900efaad4619f816455abf84e571cd61853
bit <bit@DESKTOP-487GS9K>
2020-05-11 21:17:10 +0900

    switch 변형

--- src/chapter01/SwitchTest.java
+++ src/chapter01/SwitchTest.java
@@ -9,6 +9,7 @@ public class SwitchTest {
 		int month = s.nextInt();
 		int days = 0;
 		
+		/*
 		if( month ==1 ) {
 			days = 31;
 		}
@@ -62,6 +63,40 @@ public class SwitchTest {
 		else {
 			System.out.println("1~12월 까지만 넣어주세요");
 			return;
+		}
+		*/
+		
+		switch( month ) { // break를 넣어야 switch밖으로 나가
+		case 1: //break를 안하면 밑으로 떨어져
+		case 3: 
+		case 5: 
+		case 7: 
+		case 8: 
+		case 10: 
+		case 12: {
+			days = 31;
+			break;
+		}
+		
+		case 2: {
+			days = 28;
+			break;
+		}
+		
+		case 4: 
+		case 6:
+		case 9: 
+		case 11: {
+			days = 30;
+			break;
+		}
+		
+	
+		default: { //else같은거
+			System.out.println("1~12월 까지만 넣어주세요");
+			return;
+		}
+		
 		}
 			
 		System.out.println(month + "월은" + days + "일 까지 있습니다.");
