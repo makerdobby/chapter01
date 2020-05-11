f5aaec283d734e1412f496e4915d59a30042845b
bit <bit@DESKTOP-487GS9K>
2020-05-11 20:56:51 +0900

    = vs ==

--- src/chapter01/SwitchTest.java
+++ src/chapter01/SwitchTest.java
@@ -53,7 +53,7 @@ public class SwitchTest {
 		if( month ==1 || month ==3 || month ==5 || month ==7 ||month ==8 || month ==10 || month ==12) {
 			days = 31;
 		}
-		else if(month ==4 || month = 6 ||month ==9 || month = 11 ) {
+		else if(month ==4 || month == 6 ||month ==9 || month == 11 ) {
 			days = 30;
 		}
 		else if( month == 2) {
